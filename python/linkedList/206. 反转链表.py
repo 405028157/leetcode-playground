337acaa924442b57cd9caba04c743a91560ee527
Orange <405028157@qq.com>
2021-06-05 16:23:56 +0800

    206. 反转链表 递归版本

--- python/linkedList/206. 反转链表.py
+++ python/linkedList/206. 反转链表.py
@@ -5,13 +5,12 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        if not head:
-            return None
-        last = None
-        cur = head
-        while cur: # 尽可能拿浅显和直接的变量当做循环的条件... 但是循环结束cur是None，应该返回last
-            nxt = cur.next
-            cur.next = last
-            last = cur
-            cur = nxt
-        return last
+        if not head or not head.next:
+            return head
+        
+        ret = self.reverseList(head.next)
+        # 这里下一层的递归head是本层的head.next，两层递归都修改到了同一个节点的next指针，但是由于最外层的递归最后执行，所以内层递归的head.next = null操作先执行，然后被外层的head.next.next = head覆盖，不会让链表断开
+        head.next.next = head
+        head.next = None
+        
+        return ret
