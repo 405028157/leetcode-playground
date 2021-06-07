678763a8c63eee1d63620f956d18995e8a27453e
Orange <405028157@qq.com>
2021-06-07 11:42:40 +0800

    15. 三数之和 注释

--- python/双指针/15. 三数之和.py
+++ python/双指针/15. 三数之和.py
@@ -1,3 +1,6 @@
+"""
+排完序后，对每一个固定好的first，second和third用双指针思路就可以。这道题额外需要注意的是，在循环的时候，因为可能有重复的元素，导致重复答案，所以每层循环需要看情况continue
+"""
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         ans = []
@@ -19,6 +22,7 @@ class Solution:
                 if second == third:
                     break
                 
+                # 这里只需要找到一个third就好了，因为third再往左要么就是大小相同的重复结果，要么就是变小使和 < 0
                 if nums[first] + nums[second] + nums[third] == 0:
                     ans.append([nums[first], nums[second], nums[third]])
         
