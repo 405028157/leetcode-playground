ea78574d60c3765d324cd9e952274788c40b2e25
Orange <405028157@qq.com>
2021-06-08 10:40:07 +0800

    541. 反转字符串 II 性能大提升

--- python/字符串/541. 反转字符串 II.py
+++ python/字符串/541. 反转字符串 II.py
@@ -1,23 +1,16 @@
-class Solution:
-    def reverseStr(self, s: str, k: int) -> str:
-        n = len(s)
-        left = 0
-        s = list(s)
-        while left + k < n:
-            # k 个数，应该是[left, ..., left + k - 1]
-            i, j = left, left + k - 1
-            while i < j:
-                s[i], s[j] = s[j], s[i]
-                i += 1
-                j -= 1
-            
-            left += 2 * k
-        
-        if left < n:
-            i, j = left, n - 1
-            while i < j:
-                s[i], s[j] = s[j], s[i]
-                i += 1
-                j -= 1
-        
-        return ''.join(s)
+"""
+>>> s
+['f', 's', 'd', 'a', 'f', 'd', 's', 'a']
+>>> len(s)
+8
+>>> s[7:10]
+['a']
+
+用到了切片的终点可以超出长度
+"""
+class Solution(object):
+    def reverseStr(self, s, k):
+        a = list(s)
+        for i in range(0, len(a), 2*k):
+            a[i:i+k] = reversed(a[i:i+k])
+        return "".join(a)
