6948590427c9b4440755235b22990d4464a7d0fd
Orange <405028157@qq.com>
2021-06-08 11:15:46 +0800

    151. 翻转字符串里的单词 split()不传参,多个空格和一个空格一样的,另外首尾空格也无所谓

--- python/字符串/151. 翻转字符串里的单词.py
+++ python/字符串/151. 翻转字符串里的单词.py
@@ -1,8 +1,9 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        import re
-        s = s.strip()
-        s = re.sub(r'[ ]+', ' ', s)
+        # import re
+        # s = s.strip()
+        # s = re.sub(r'[ ]+', ' ', s)
         
-        array_s = s.split(' ')
+        array_s = s.split()
+        # print(array_s)
         return ' '.join(array_s[::-1])
