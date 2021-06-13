0fc1af98c071a6a2b63a252ed75a5dcbeca99e9d
Orange <405028157@qq.com>
2021-06-13 18:45:03 +0800

    225. 用队列实现栈 push时间复杂度高，只使用一个队列解法, 入栈操作 O(n)O(n)，其余操作都是 O(1)。

--- python/栈/225. 用队列实现栈.py
+++ python/栈/225. 用队列实现栈.py
@@ -1,7 +1,3 @@
-"""
-和之前提交的区别是，在入队列时，就让元素保持了“栈”应有的顺序，所以push时间复杂度高，但是pop, top时间复杂度低
-参考https://leetcode-cn.com/problems/implement-stack-using-queues/solution/yong-dui-lie-shi-xian-zhan-by-leetcode-solution/
-"""
 from collections import deque
 class MyStack:
 
@@ -9,36 +5,44 @@ class MyStack:
         """
         Initialize your data structure here.
         """
-        self.queue1 = deque()
-        self.queue2 = deque()
+        self.queue = deque()
 
 
     def push(self, x: int) -> None:
         """
         Push element x onto stack.
         """
-        self.queue2.append(x)
-        while self.queue1:
-            self.queue2.append(self.queue1.popleft())
-        self.queue1, self.queue2 = self.queue2, self.queue1
+        n = len(self.queue)
+        self.queue.append(x)
+        
+        for _ in range(n):
+            self.queue.append(self.queue.popleft())
 
 
     def pop(self) -> int:
         """
         Removes the element on top of the stack and returns that element.
         """
-        return self.queue1.popleft()
-
+        return self.queue.popleft()
 
     def top(self) -> int:
         """
         Get the top element.
         """
-        return self.queue1[0]
+        return self.queue[0]
 
 
     def empty(self) -> bool:
         """
         Returns whether the stack is empty.
         """
-        return not self.queue1
+        return not bool(self.queue)
+
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
