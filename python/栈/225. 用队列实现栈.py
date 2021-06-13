dba7964fbde7b73c8678b5cc3aeed4f44bf9d18d
Orange <405028157@qq.com>
2021-06-13 18:12:11 +0800

    225. 用队列实现栈 push实现错误

---
+++ python/栈/225. 用队列实现栈.py
@@ -0,0 +1,72 @@
+from collections import deque
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue1 = deque()
+        self.queue2 = deque()
+
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        while self.queue1:
+            self.queue2.append(self.queue1.popleft())
+        
+        self.queue1.append(x)
+
+        while self.queue2:
+            self.queue1.append(self.queue2.popleft())
+        
+        # print(f'stack1 after push: {self.queue1}')
+
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        last = None
+        while self.queue1:
+            last = self.queue1.popleft()
+            # 如果last不是最后一个元素，不需要删，放回queue2队列
+            if self.queue1:
+                self.queue2.append(last)
+        
+        while self.queue2:
+            self.queue1.append(self.queue2.popleft())
+
+        # print(f'stack1 after pop: {self.queue1}')
+        return last
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        last = None
+        while self.queue1:
+            last = self.queue1.popleft()
+            self.queue2.append(last)
+        while self.queue2:
+            self.queue1.append(self.queue2.popleft())
+        
+        # print(f'stack1 after top: {self.queue1}')
+        return last
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return not bool(self.queue1)
+
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
