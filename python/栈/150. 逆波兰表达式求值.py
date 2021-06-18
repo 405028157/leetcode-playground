3cdd42bb85818734da2c3faf5dc35a3b3c5e76ab
Orange <405028157@qq.com>
2021-06-18 10:34:39 +0800

    150. 逆波兰表达式求值 bug, 除法实现错了

--- python/栈/150. 逆波兰表达式求值.py
+++ python/栈/150. 逆波兰表达式求值.py
@@ -13,8 +13,8 @@ class Solution:
             try:
                 num = int(token)
             except:
-                num1 = stack.pop()
                 num2 = stack.pop()
+                num1 = stack.pop()
                 num = operator_hash[token](num1, num2)
                 print(f'token = {token}, num1 = {num1}, num2 = {num2}, num = {num}')
             stack.append(num)
