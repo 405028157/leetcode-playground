adbee0b60e72d75c3f87d7e1b7fe5da9b675e9a2
Orange <405028157@qq.com>
2021-06-18 10:32:21 +0800

    150. 逆波兰表达式求值 bug, num1, num2操作数顺序错了

---
+++ python/栈/150. 逆波兰表达式求值.py
@@ -0,0 +1,22 @@
+import math
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operator_hash = {
+            '+': lambda x, y: x + y,
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: math.floor(x / y)
+        }
+
+        stack = []
+        for token in tokens:
+            try:
+                num = int(token)
+            except:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                num = operator_hash[token](num1, num2)
+                print(f'token = {token}, num1 = {num1}, num2 = {num2}, num = {num}')
+            stack.append(num)
+        
+        return stack.pop()
