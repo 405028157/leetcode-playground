fcf80d1c62e3350e87ba32461e748f8bd951e991
xiaotian <405028157@qq.com>
2021-06-21 10:51:49 +0800

    226. 翻转二叉树 bfs解法

--- python/tree/226. 翻转二叉树.py
+++ python/tree/226. 翻转二叉树.py
@@ -1,3 +1,9 @@
+"""
+BFS 解法，这道题本质是让每个节点的左右孩子都互换一下
+"""
+
+import collections
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -6,13 +12,17 @@ class TreeNode:
         self.right = right
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
-        def dfs(node: TreeNode):
-            if not node:
-                return
+        if not root:
+            return
+        q = collections.deque()
+        q.append(root)
 
+        while q:
+            node = q.popleft()
             node.left, node.right = node.right, node.left
-            dfs(node.left)
-            dfs(node.right)
 
-        dfs(root)
+            # 这里left，right节点和原来的互换，但是不影响
+            node.left and q.append(node.left)
+            node.right and q.append(node.right)
+        
         return root
