8414ad4b0ac09d31d2f7fa3545e06965b45b7ae5
Orange <405028157@qq.com>
2021-06-22 16:34:41 +0800

    112. 路径总和 不用辅助dfs函数

--- python/tree/112. 路径总和.py
+++ python/tree/112. 路径总和.py
@@ -8,13 +8,18 @@ class Solution:
     def __init__(self):
         self.sum = 0
     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        # print(self.sum)
         if not root:
             return False
         
         self.sum += root.val
         # 到达叶子节点
         if not(root.left or root.right):
-            return self.sum == targetSum
+            temp = self.sum
+            self.sum -= root.val
+            return temp == targetSum
         
-        
-        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+        l = self.hasPathSum(root.left, targetSum)
+        r = self.hasPathSum(root.right, targetSum)
+        self.sum -= root.val
+        return l or r
