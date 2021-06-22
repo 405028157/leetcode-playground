9465be24f7bdf25a0f7d6d53b00064323c281834
Orange <405028157@qq.com>
2021-06-22 16:31:15 +0800

    112. 路径总和 不用辅助dfs函数, 含bug

---
+++ python/tree/112. 路径总和.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def __init__(self):
+        self.sum = 0
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        self.sum += root.val
+        # 到达叶子节点
+        if not(root.left or root.right):
+            return self.sum == targetSum
+        
+        
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
