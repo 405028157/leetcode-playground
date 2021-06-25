c32039dcf80635fce406a7c5bb5cb18937876e65
Orange <405028157@qq.com>
2021-06-25 16:12:20 +0800

    669. 修剪二叉搜索树 增加官方标答

--- python/tree/669. 修剪二叉搜索树.py
+++ python/tree/669. 修剪二叉搜索树.py
@@ -31,3 +31,22 @@ class Solution:
                 root = root.left
         
         return root
+
+"""
+官方非常快的解法，有点像二分
+class Solution(object):
+    def trimBST(self, root, L, R):
+        def trim(node):
+            if not node:
+                return None
+            elif node.val > R:
+                return trim(node.left)
+            elif node.val < L:
+                return trim(node.right)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+
+        return trim(root)
+"""
