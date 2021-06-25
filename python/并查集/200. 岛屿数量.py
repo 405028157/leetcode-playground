6db0822433ece24a374da084f3457857a2fae21c
Orange <405028157@qq.com>
2021-06-25 20:26:38 +0800

    200. 岛屿数量 添加DFS解法

--- python/并查集/200. 岛屿数量.py
+++ python/并查集/200. 岛屿数量.py
@@ -55,7 +55,28 @@ grid = [
 s.numIslands(grid)
 
 
+"""
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(x, y):
+            grid[x][y] = '0'
+            # 看看空格怎么加
+            if x - 1 >= 0 and grid[x-1][y] == '1': dfs(x - 1, y)
+            if x + 1 < m and grid[x+1][y] == '1': dfs(x + 1, y)
+            if y - 1 >= 0 and grid[x][y - 1] == '1': dfs(x, y - 1)
+            if y + 1 < n and grid[x][y + 1] == '1': dfs(x, y + 1)
+        
+        m, n = len(grid), len(grid[0])
+        num_islands = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    num_islands += 1
+        
+        return num_islands
 
+"""
 
 
 
