2a619c64f597b1aee8aa7f02be8021dce9bb0e47
Orange <405028157@qq.com>
2021-06-26 08:51:57 +0800

    538. 把二叉搜索树转换为累加树 更优解法 根据递归转成迭代

--- python/tree/538. 把二叉搜索树转换为累加树.py
+++ python/tree/538. 把二叉搜索树转换为累加树.py
@@ -9,21 +9,42 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def convertBST(self, root: TreeNode) -> TreeNode:
+    # 递归
+    def convertBST2(self, root: TreeNode) -> TreeNode:
         if not root:
             return None
 
-        pre = None
+        total = 0
         def dfs(root: TreeNode):
-            nonlocal pre
+            nonlocal total
             if not root:
                 return None
             
             dfs(root.right) # 这里可以不用接收返回值
-            if pre != None:
-                root.val = root.val + pre
-            pre = root.val
+            total += root.val
+            root.val = total
             dfs(root.left) # 这里可以不用接收返回值
             return root
         
         return dfs(root)
+    
+    # 迭代
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        
+        stack = []
+        cur = root
+        total = 0
+
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.right
+            
+            top = stack.pop()
+            total += top.val
+            top.val = total
+            cur = top.left
+        
+        return root
