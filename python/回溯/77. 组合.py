0b62d4f1d1693ac7d40b08266189fe95e194947b
Orange <405028157@qq.com>
2021-06-28 14:54:35 +0800

    77. 组合 画法2, 俩个分支的树, 增加画法1更舒服解法

--- python/回溯/77. 组合.py
+++ python/回溯/77. 组合.py
@@ -1,3 +1,6 @@
+"""
+画法2
+"""
 class Solution:
     def combine(self, n: int, k: int) -> list[list[int]]:
         combination = []
@@ -25,3 +28,35 @@ class Solution:
         
         dfs(1)
         return ans
+
+"""
+画法1
+
+class Solution:
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        combination = []
+        ans = []
+
+        def dfs(cur: int):
+            if len(combination) == k:
+                ans.append(combination[:])
+                return
+            
+            # 每层做下层的决定，所以 cur > n，说明到最底层了
+            if cur > n:
+                return
+            
+            # 剪枝
+            # [cur, ..., n]
+            if n - cur + 1 + len(combination) < k:
+                return
+            
+
+            for i in range(cur, n + 1):
+                combination.append(i)
+                dfs(i + 1)
+                combination.pop()
+        
+        dfs(1)
+        return ans
+"""
