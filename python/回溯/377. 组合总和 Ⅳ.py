fa30331f7cc88420542cba8ecd96f723232b75ba
Orange <405028157@qq.com>
2021-06-30 10:00:23 +0800

    377. 组合总和 Ⅳ  wrong answer

---
+++ python/回溯/377. 组合总和 Ⅳ.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum4(self, nums: list[int], target: int) -> int:
+        n = len(nums)
+        ans = 0
+        
+
+        def dfs(index: int, rest:int):
+            nonlocal ans
+            if rest == 0:
+                ans += 1
+                return
+
+            # 题目没说整数不可以是负数，先当做不可以吧
+            if rest < 0:
+                return
+            
+            if index >= n:
+                return
+
+            dfs(index + 1, rest)
+            dfs(index, rest - nums[index])
+        
+        dfs(0, target)
+        return ans
+            
+                
