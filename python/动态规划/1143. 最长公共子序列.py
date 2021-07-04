66983f47d59dd644bd0ee18203b470e151d22d56
Orange <405028157@qq.com>
2021-07-04 21:53:14 +0800

    1143. 最长公共子序列

--- python/动态规划/1143. 最长公共子序列.py
+++ python/动态规划/1143. 最长公共子序列.py
@@ -15,9 +15,13 @@ class Solution:
         # i = [1, m], j = [1, n]
         for i in range(1, m+1):
             for j in range(1, n+1):
-                if text1[i] == text2[j]:
+                if text1[i-1] == text2[j-1]:
                     dp[i][j] = dp[i - 1][j - 1] + 1
                 else:
                     dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
         
         return dp[m][n]
+
+"""
+注意dp[i][j]指的是长度为i和长度为j的字符串，的最长公共子序列。对应到text1和text2的下标，应该是i-1，和j-1
+"""
