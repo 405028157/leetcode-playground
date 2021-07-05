04da096b42fb14a0f81e52ae38c0e95dcc02dc71
Orange <405028157@qq.com>
2021-07-05 19:51:27 +0800

    377. 组合总和 Ⅳ 动态规划解法

--- python/回溯/377. 组合总和 Ⅳ.py
+++ python/回溯/377. 组合总和 Ⅳ.py
@@ -1,3 +1,7 @@
+"""
+超出时间限制
+"""
+
 class Solution:
     def combinationSum4(self, nums: list[int], target: int) -> int:
         ans = 0
@@ -20,3 +24,22 @@ class Solution:
         return ans
             
                 
+"""
+动态规划，正确思路
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        n = len(nums)
+        nums.sort()
+
+        dp[0] = 1
+        for i in range(1, target + 1):
+            for j in range(n):
+                if i - nums[j] >= 0:
+                    dp[i] += dp[i - nums[j]]
+                else:
+                    break
+        
+        return dp[target]
+"""
