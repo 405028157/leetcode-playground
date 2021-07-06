0249514a8c3ed058712111de44fd9c339edf0483
Orange <405028157@qq.com>
2021-07-06 09:43:02 +0800

    93. 复原 IP 地址 改进

--- python/回溯/93. 复原 IP 地址.py
+++ python/回溯/93. 复原 IP 地址.py
@@ -4,24 +4,23 @@ class Solution:
         res = []
 
         def dfs(combination, start):
-            # 用完了所有字符串，但是没有组成四段ip
-            if start >= n and len(combination) < 4:
+            if start >= n:
+                # 恰好用完字符串组成四段ip
+                if start == n and len(combination) == 4:
+                    res.append('.'.join(combination))
                 return
 
             # 有了四段ip，但是没有用完字符串，说明是无效的组合
             if len(combination) == 4 and start < n - 1:
                 return
-            
-            # 恰好用完字符串组成四段ip
-            if len(combination) == 4 and start == n:
-                res.append('.'.join(combination))
 
             # i = [1,3]
             for i in range(1, 4):
                 temp = s[start: start + i]
-                # 切片超出范围，temp为空
-                if not temp:
-                    break
+                # # 切片超出范围，temp为空
+                # if not temp:
+                #     print(start, i, n)
+                #     break
                 if int(temp) > 255:
                     break
                 if len(temp) > 1 and temp[0] == '0':
