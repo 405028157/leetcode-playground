9f04770a48ef1cf569a567225b3b932e6db83cfd
Orange <405028157@qq.com>
2021-07-18 10:48:18 +0800

    494. 目标和 改bug

--- python/动态规划/494. 目标和.py
+++ python/动态规划/494. 目标和.py
@@ -8,19 +8,20 @@ class Solution:
         n = len(nums)
         ans = 0
 
-        dp = [[0] * 2001 for _ in range(n + 1)]
+        dp = [[0] * 3001 for _ in range(n + 1)]
 
         dp[0][0 + 1000] = 1
         
         for i in range(0, 1000):
             dp[0][i] = 0
         
-        for i in range(1001, 2001):
+        for i in range(1001, 3001):
             dp[0][i] = 0
         
         for i in range(1, n + 1):
-            for j in range(1001, target + 1001):
-                dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j + nums[i - 1]]
+            for j in range(-1000, 1001):
+                dp[i][j + 1000] = dp[i - 1][j - nums[i - 1] + 1000] + dp[i - 1][j + nums[i - 1] + 1000]
+
         return dp[n][target + 1000]
         
 
