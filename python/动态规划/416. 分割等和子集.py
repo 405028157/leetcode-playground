51bce7fb5a52e41763e9c2a4c01d37fa7c1cac21
Orange <405028157@qq.com>
2021-07-18 09:25:31 +0800

    416. 分割等和子集 优化

--- python/动态规划/416. 分割等和子集.py
+++ python/动态规划/416. 分割等和子集.py
@@ -23,3 +23,24 @@ class Solution:
                     dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1])
         
         return dp[n][C] != float('-inf')
+
+"""
+如果灵活一点，float('-inf')其实就是为了传递负无穷，没必要关心float('-inf') + nums[i - 1]，甚至可以设置成 False
+再加入空间优化
+"""
+
+class Solution:
+    def canPartition(self, nums: list[int]) -> bool:
+        total, n = sum(nums), len(nums)
+        if total % 2 == 1 or n == 1:
+            return False
+        
+        C = total >> 1
+        dp = [False for _ in range(C + 1)]
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(C, nums[i - 1] - 1, -1):
+                dp[j] = dp[j] or dp[j - nums[i - 1]]
+        
+        return dp[C]
