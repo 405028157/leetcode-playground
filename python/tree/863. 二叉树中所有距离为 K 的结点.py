a9e70c2c954b39b46590003d02e96580d63b0e35
Orange <405028157@qq.com>
2021-07-29 21:53:32 +0800

    863. 二叉树中所有距离为 K 的结点

--- python/tree/863. 二叉树中所有距离为 K 的结点.py
+++ python/tree/863. 二叉树中所有距离为 K 的结点.py
@@ -27,17 +27,19 @@ class Solution:
         visited = {}
         # 对于出发点target，到另一个node，只可能有一种路径和对应的一个distance，因此访问过一个节点，就不用去访问了
         def search(node: TreeNode, distance: int):
+            if not node:
+                return
             visited[node] = True # 这里也可以让节点值作为key，因为树的每个节点值都不同
 
             if distance == k:
                 res.append(node.val)
                 return
             
-            if not visited[node.left]:
+            if node.left not in visited:
                 search(node.left, distance + 1)
-            if not visited[node.right]:
+            if node.right not in visited:
                 search(node.right, distance + 1)
-            if not visited[dic[node]]:
+            if node in dic and dic[node] not in visited: # 注意这里，树的root节点没有父节点，所以不在dic里
                 search(dic[node], distance + 1)
         
         search(target, 0)
